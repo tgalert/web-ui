11159631967fbbe38460769334df2074026ec52c
Daniel Weibel <danielmweibel@gmail.com>
2018-10-11 22:25:04 +0200

    Integrate AWS Amplify (several required fixes)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AmplifyService } from 'aws-amplify-angular';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    AmplifyService
+  ]
 })
 export class CoreModule { }
