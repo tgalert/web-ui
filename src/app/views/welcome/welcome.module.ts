3345f807403065fdda100f21e58eabc69fd2e9ae
Daniel Weibel <danielmweibel@gmail.com>
2018-10-11 20:55:49 +0200

    Add basic module structure
    - app
        - core (module)
        - shared (module)
        - views
            - dashboard (module, lazy-loaded)
            - welcome (module, lazy-loaded)

---
+++ src/app/views/welcome/welcome.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WelcomeRoutingModule } from './welcome-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule
+  ],
+  declarations: []
+})
+export class WelcomeModule { }
