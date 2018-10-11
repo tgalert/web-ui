e8471defab06704076dbf6449d09a1000d49a145
Daniel Weibel <danielmweibel@gmail.com>
2018-10-11 21:13:27 +0200

    Include Angular Material
    And create shared module 'material' that re-exports common Angular
    Material modulesInclude Angular Material
    
    And create shared module 'material' that re-exports common Angular
    Material modules..

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatTabsModule
+} from '@angular/material';
+
+@NgModule({
+  exports: [
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDialogModule,
+  ]
+})
+export class MaterialModule { }
