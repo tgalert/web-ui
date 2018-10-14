7e793fee4a8c0fa5d9c5083cd9deebd732640fcb
Daniel Weibel <danielmweibel@gmail.com>
2018-10-14 18:05:44 +0200

    Add Amplify Hosting (dev on S3 without CloudFront)

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,10 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "us-east-1:426bebcf-2696-4245-bac5-e199ce7a5ef0",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_caEtzyjHJ",
-    "aws_user_pools_web_client_id": "dcvoqr2l6ibq6hn6rnlt66man"
+    "aws_user_pools_web_client_id": "dcvoqr2l6ibq6hn6rnlt66man",
+    "aws_content_delivery_bucket": "moduletest-20181014175315--hostingbucket",
+    "aws_content_delivery_bucket_region": "us-east-1",
+    "aws_content_delivery_url": "http://moduletest-20181014175315--hostingbucket.s3-website-us-east-1.amazonaws.com"
 };
 
 
